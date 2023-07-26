d4aed98eab12cbd73017049fe6b724f3336dd41a
Ivan Gonzalez <6606619+gonzalezi2@users.noreply.github.com>
2023-07-25 22:48:56 -0700

    feat: programatically add blocks to game board

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 'use strict';
 
-// Game elements
+/**
+ * Game elements
+ */
 const gameGrid = document.getElementById('game');
 const timerElement = document.getElementById('timer');
 const flags = document.getElementById('flags');
 const resetBtn = document.getElementById('reset');
 let isGameStart = false;
-let counter = 0;
 let interval;
 
-// Constants
+/**
+ * Game variables
+ */
 const difficulty = 'easy';
 const gameMatrixSize = {
   easy: {
@@ -17,19 +20,24 @@ const gameMatrixSize = {
     cols: 9,
   },
 };
-let minesAndFlags = 10;
+let counter = 0;
+let numOfGuessesLeft = 10;
 const gameMatrix = Array(gameMatrixSize[difficulty].rows)
   .fill()
   .map(() => Array(gameMatrixSize[difficulty].cols).fill());
 
-// Event listeners
+const numOfGameBlocks = gameMatrixSize[difficulty].rows * gameMatrixSize[difficulty].cols;
+
+/**
+ * Event listeners
+ */
 resetBtn.addEventListener('click', resetGameHandler);
 gameGrid.addEventListener('click', blockClickHandler);
 gameGrid.addEventListener('contextmenu', rightClickHandler);
 
 let mineLocations = [];
 
-while (mineLocations.length < minesAndFlags) {
+while (mineLocations.length < numOfGuessesLeft) {
   let randomGridLocation = getRandomGridLocations();
   if (mineLocations.indexOf(randomGridLocation) < 0) {
     mineLocations.push(randomGridLocation);
@@ -42,16 +50,29 @@ function getRandomGridLocations() {
   return `${randomRow}${randomCol}`;
 }
 
+function createGameBlocks() {
+  gameGrid.innerHTML = '';
+  for (let i = 0; i < numOfGameBlocks; i += 1) {
+    const newBlock = document.createElement('div');
+    newBlock.classList.add('block');
+    gameGrid.append(newBlock);
+  }
+}
+
 function rightClickHandler(e) {
   e.preventDefault();
 
-  if (e.target.classList.contains('flagged')) {
-    minesAndFlags += 1;
-  } else {
-    minesAndFlags -= 1;
+  if (!e.target.classList.contains('block')) {
+    return;
   }
 
-  flags.innerText = `${minesAndFlags.toString().padStart(3, '0')}`;
+  if (e.target.classList.contains('flagged')) {
+    numOfGuessesLeft += 1;
+  } else {
+    numOfGuessesLeft -= 1;
+  }
+
+  flags.innerText = `${numOfGuessesLeft.toString().padStart(3, '0')}`;
   e.target.classList.toggle('flagged');
 }
 
@@ -79,12 +100,15 @@ function blockClickHandler(e) {
 function resetGameHandler() {
   clearInterval(interval);
   counter = 0;
-  minesAndFlags = 0;
+  numOfGuessesLeft = 0;
   isGameStart = false;
   interval = undefined;
   timer.innerText = '000';
-  flags.innerText = `${minesAndFlags.toString().padStart(3, '0')}`;
+  flags.innerText = `${numOfGuessesLeft.toString().padStart(3, '0')}`;
+  createGameBlocks();
 }
+
+createGameBlocks();
 
 // Recursive function to clear all the empty areas that are adjacent to the selected empty block
 // function clearAdjacentEmptyAreaas()
